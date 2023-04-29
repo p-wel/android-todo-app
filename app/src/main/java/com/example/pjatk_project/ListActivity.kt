903b1b40195a261013e5243bce3dbe970d3681aa
p-wel <wojtowiczp97@gmail.com>
2023-04-29 22:21:39 +0200

    fix example tasks

--- app/src/main/java/com/example/pjatk_project/ListActivity.kt
+++ app/src/main/java/com/example/pjatk_project/ListActivity.kt
@@ -26,16 +26,16 @@ class ListActivity : AppCompatActivity() {
             Task("Task 1"),
             Task("Task 2"),
             Task("Task 3"),
-            Task("Task 1"),
-            Task("Task 2"),
-            Task("Task 3"),
-            Task("Task 1"),
-            Task("Task 2"),
-            Task("Task 3"),
-            Task("Task 1"),
-            Task("Task 2"),
-            Task("Task 3"),
-            Task("Task 4")
+            Task("Task 5"),
+            Task("Task 6"),
+            Task("Task 7"),
+            Task("Task 8"),
+            Task("Task 9"),
+            Task("Task 10"),
+            Task("Task 11"),
+            Task("Task 12"),
+            Task("Task 13"),
+            Task("Task 14")
         )
 
 
